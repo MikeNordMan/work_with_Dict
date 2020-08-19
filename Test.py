c43a89bed3f0889ada6d2b869f18d3bba18708fc
MikeNordMan <49362861+MikeNordMan@users.noreply.github.com>
2020-08-19 12:28:27 +0500

    Ok

---
+++ Test.py
@@ -0,0 +1,27 @@
+
+
+
+class tastDict():
+
+    '''Конструктор'''
+    def __init__(self, dict):
+        self.dict = dict
+
+    '''Возвращение Массива ключей'''
+    def returnKey(self):
+        pass
+
+    '''Возврашение массива данных'''
+    def returnData(self):
+        pass
+
+    '''Добавление в словарь элемента'''
+    def addElementDict(self):
+        pass
+
+    ''' Удаление элементо Словаря '''
+    def delitElemrntDict(self):
+        pass
+
+
+
